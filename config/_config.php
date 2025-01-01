7923508b427bd8bcdbb43d724b269d18f67dbb49
Solene Baudic <solene.baudic@gmail.com>
2025-01-01 14:53:17 +0100

    Feat - accessible la config

---
+++ config/_config.php
@@ -0,0 +1,11 @@
+<?php
+
+
+
+define('TEMPLATE_VIEW_PATH', './Views/Pages/');
+define('MAIN_VIEW_PATH', TEMPLATE_VIEW_PATH . 'main.php');
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'tom_troc');
+define('DB_USER', 'root');
+define('DB_PASS', '');
